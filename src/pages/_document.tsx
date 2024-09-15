e9349b3d92eea8672e31724024a05e3ecd4d13c8
Daniel Ström <D@nielstrom.se>
2024-09-15 15:17:59 +0200

    Link to manifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document(): JSX.Element {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
