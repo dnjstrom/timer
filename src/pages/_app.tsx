f920829283c2c87ace86c900ce41b9749004d4fd
Daniel Ström <D@nielstrom.se>
2024-09-16 07:38:43 +0200

    Add insets for iphone safe areas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import type { AppProps } from 'next/app';
 import '@styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="initial-scale=1, viewport-fit=cover" />
+        <link rel="manifest" href="manifest.json" />
+      </Head>
+      <Component {...pageProps} />;
+    </>
+  );
 }
